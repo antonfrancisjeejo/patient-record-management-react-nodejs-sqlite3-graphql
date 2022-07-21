dd45259873b9b7c606e21f553f70731e460fcfcf
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-07-21 19:40:27 +0530

    Initial commit

--- server/query/index.js
+++ server/query/index.js
@@ -1,37 +1,6 @@
 const PatientType = require("../schema/patient");
-const patientsDb = require("../services/patientsDb");
 const { GraphQLObjectType, GraphQLList, GraphQLString } = require("graphql");
-const formatData = require("../utils/format");
-
-function allPatients() {
-  return new Promise(function (resolve, reject) {
-    patientsDb.all(
-      "SELECT patients.name,patients.gender,patients.address,patients.phone_number,patients.age, patients.doctor,patients.date,patients.amount,treatments.disease, treatments.category, treatments.prescriptions FROM Patients join treatments on patients.treatment_id = treatments.id",
-      [],
-      (err, response) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(formatData(response));
-      }
-    );
-  });
-}
-
-function getPatient(category) {
-  return new Promise(function (resolve, reject) {
-    patientsDb.all(
-      `SELECT patients.name,patients.gender,patients.address,patients.phone_number,patients.age, patients.doctor,patients.date,patients.amount,treatments.disease, treatments.category, treatments.prescriptions FROM Patients join treatments on patients.treatment_id = treatments.id where treatments.category = ?`,
-      [category],
-      (err, response) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(formatData(response));
-      }
-    );
-  });
-}
+const { allPatients, getPatient } = require("../lib/patient");
 
 const query = new GraphQLObjectType({
   name: "RootQuery",
